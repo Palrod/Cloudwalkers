d3ae2420f95162905af8d54f55487779de205e11
Palrod <147259835+Palrod@users.noreply.github.com>
2023-11-14 02:02:14 +0000

    Add files via upload

---
+++ 2-Subnets.tf
@@ -0,0 +1,85 @@
+#These are   for  public
+
+resource "aws_subnet" "public-ap-southeast-1a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.16.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-southeast-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "public-ap-southeast-1b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.16.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-southeast-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_subnet" "public-ap-southeast-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.16.3.0/24"
+  availability_zone       = "ap-southeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-southeast-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-ap-southeast-1a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.16.11.0/24"
+  availability_zone = "ap-southeast-1a"
+
+  tags = {
+    Name    = "private-ap-southeast-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "private-ap-southeast-1b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.16.12.0/24"
+  availability_zone = "ap-southeast-1b"
+
+  tags = {
+    Name    = "private-ap-southeast-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_subnet" "private-ap-southeast-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.16.13.0/24"
+  availability_zone = "ap-southeast-1c"
+
+  tags = {
+    Name    = "private-ap-southeast-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
